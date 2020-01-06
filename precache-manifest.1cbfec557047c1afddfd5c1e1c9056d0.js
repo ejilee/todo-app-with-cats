68d9b2268ce771175efaf7292160160c85d2fef7
ejilee <whyejile@gmail.com>
2020-01-06 18:00:08 +0900

    Updates

---
+++ precache-manifest.1cbfec557047c1afddfd5c1e1c9056d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b19e5ac42a0597184ad9e377fab9e3a",
+    "url": "/todo-app-with-cats/index.html"
+  },
+  {
+    "revision": "5b71d223859c1a43e00e",
+    "url": "/todo-app-with-cats/static/css/main.bb31ae57.chunk.css"
+  },
+  {
+    "revision": "29481975221e3625467e",
+    "url": "/todo-app-with-cats/static/js/2.93a6fa06.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-app-with-cats/static/js/2.93a6fa06.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b71d223859c1a43e00e",
+    "url": "/todo-app-with-cats/static/js/main.95f13b27.chunk.js"
+  },
+  {
+    "revision": "3029dd968fa2299bc0b5",
+    "url": "/todo-app-with-cats/static/js/runtime-main.87ed8152.js"
+  }
+]);
